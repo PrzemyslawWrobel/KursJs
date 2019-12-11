5441960957d30fcb4cdad813bc4f32d738231905
PW <przemek.wrobel@poczta.fm>
2019-12-11 10:38:49 +0100

    Tablice

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,61 @@
-var age = prompt('podaj wiek kota: ');
+/*  
+ * Arrays - z ang. TABLICE
+ * 
+ * TAB
+ * ------------------------------------------------
+ *   [0]  |  [1]  |  [2]  |  [3]  |  [4]  |  [5]  |
+ * ------------------------------------------------
+ *          
+ */
 
-var div = document.getElementById('kot');
+var produkty = [
+    "PHP",
+    "Mysql",
+    "Javascript"
+];
 
-div.innerHTML = "wiek Twojego to: " + age;
+produkty[3] = "PDO";
+
+// var element = document.getElementById("kursyProgramowania");
+
+// element.innerHTML = produkty;
+
+
+//produkty.push("PDO");
+
+//var produkty = new Array();
+
+//alert(produkty);
+
+
+//  tablice asocjacyjne to rodzaj tablic gdzie zamiast indeksu jest string
+//ASSOCIATIVE ARRAYS
+
+var osoba = [];
+osoba["imie"] = "Arek";
+osoba["nazwisko"] = "Nazwisko";
+//alert(osoba); //lub
+//alert(osoba.imie);
+
+var ul = document.getElementById("kursyTworzeniaStronWWW");
+alert(ul);
+
+// var kursyProgramowania = document.getElementsByTagName("li");
+
+// alert(kursyProgramowania[6].innerHTML);
+
+// //var age = prompt('podaj wiek kota: ');
+
+// var div = document.getElementById("kursyProgramowania");
+
+// div.innerHTML = "nowa treść";
+
+// var osoba = {
+//     imie: "Bartek",
+//     nazwisko: "Kurek",
+//     wiek: "42",
+//     pobinfo: function () {
+//         return this.imie + " " + this.nazwisko + " Twój wiek to " + this.wiek;
+//     }
+
+// };
